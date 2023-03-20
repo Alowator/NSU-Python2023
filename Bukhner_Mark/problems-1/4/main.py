02274d9a885dc785ec4cb721e8cc7d15a6402d01
Mark Bukhner <markbuxner@mail.ru>
2023-03-20 20:10:21 +0700

    problems-1/task4 completed

---
+++ Bukhner_Mark/problems-1/4/main.py
@@ -0,0 +1,41 @@
+def print_line(line: str, times: int = 1):
+    while times > 0:
+        print(line.capitalize())
+        times -= 1
+
+
+def main():
+    # 30 символов сырого текста.
+    a, b, c, d, e, f, g, h, i, g, k, l, m, n, o, p, q, r, s, t, u, v, y, w, x, y, z, _ = \
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'y', 'w', 'x', 'y', 'z', ' '
+    _p, _q, _c, _v = '.', "'", ',', '!'
+
+
+    print_line(t + e + n + _ + g + r + e + e + n + _ + b + o + t + t + l + e + s + _ + s + i + t + t + i + n + g + _ + o + n + _ + t + h + e + _ + w + a + l + l, 2)
+    print_line(a + n + d + _ + i + f + _ + o + n + e + _ + g + r + e + e + n + _ + b + o + t + t + l + e)
+    print_line(s + h + o + u + l + d + _ + a + c + c + i + d + e + n + t + a + l + l + y + _ + f + a + l + l + _p + _p + _p)
+    print()
+    print_line(f + o + r + _ + g.upper() + o + d + _q + s + _ + s + a + k + e + _c + _ + p + i + c + k + _ + i + t + _ + u + p + _ + a + n + d + _ + p + u + t + _ + i + t + _ + b + a + c + k + _v)
+    print_line(t + h + e + _ + d.upper() + u + k + e + _ + i + s + _ + c + o + m + i + n + g + _ + r + o + u + n + d)
+    print_line(i + t + _ + t + o + o + k + _ + m + e + _ + a + g + e + s + _ + p + u + t + t + i + n + g + _ + a + l + l + _ + t + h + e + s + e)
+    print_line(b + o + t + t + l + e + s + _ + o + n + _ + t + h + e + _ + w + a + l + l)
+    print()
+    print_line(a + n + d + _ + i + f + _ + t + h + e + _ + m.upper() + a + s + t + e + r + _ + s + e + e + s + _ + o + n + e + _ + b + o + t + t + l + e)
+    print_line(e + v + e + n + _ + s + l + i + g + h + t + l + y + _ + o + u + t + _ + o + f + _ + p + l + a + c + e)
+    print_line(h + e + _q + s + _ + g + o + i + n + g + _ + t + o + _ + y + e + l + l + _ + a + t + _ + u + s + _ + a + n + d + _ + p + u + n + i + s + h + _ + u + s)
+    print_line(a + n + d + _ + l + o + c + k + _ + u + s + _ + i + n + _ + t + h + e + _ + c + e + l + l + a + r)
+    print_line(s + o + _ + b + e + _ + c + a + r + e + f + u + l + _ + w + i + t + h + _ + t + h + o + s + e + _ + b + o + t + t + l + e + s)
+    print_line(i + f + _ + y + o + u + _ + b + r + e + a + k + _ + o + n + e + _c + _ + _ + i + f + _ + y + o + u + _ + b + r + e + a + k + _ + o + n + e)
+    print_line(t + h + e + n + _ + h + e + _q + l + l + _ + k + i + l + l + _ + u + s + _ + _c + _ + d + o + n + _q + t + _ + y + o + u + _ + u + n + d + e + r + s + t + a + n + d + '?')
+    print_line(h + e + _q + l + l + _ + a + c + t + u + a + l + l + y + _ + k + i + l + l + _ + u + s + _v)
+    print()
+    print_line(h + e + _q + l + l + _ + s + l + i + c + e + _ + u + s + _ + a + n + d + _ + h + e + _q + l + l + _ + d + i + c + e + _ + u + s)
+    print_line(a + n + d + _ + h + e + _q + l + l + _ + c + u + t + _ + u + s + _ + i + n + t + o + _ + r + i + b + b + o + n + s)
+    print_line(a + n + d + _ + h + e + _q + l + l + _ + t + e + a + r + _ + o + f + f + _ + b + o + t + h + _ + o + u + r + _ + e + y + e + l + i + d + s)
+    print_line(a + n + d + _ + h + e + _q + l + l + _ + p + u + t + _ + t + h + e + m + _ + b + a + c + k + _ + t + h + e + _ + w + r + o + n + g + _ + w + a + y + _ + r + o + u + n + d + _v)
+    print()
+    print_line(3 * _p + a + n + d + _ + t + h + e + r + e + _q + l + l + _ + b + e + _ + n + i + n + e + _ + g + r + e + e + n + _ + b + o + t + t + l + e + s + _ + s + i + t + t + i + n + g + _ + o + n + _ + t + h + e + _ + w + a + l + l)
+
+
+if __name__ == '__main__':
+    main()
